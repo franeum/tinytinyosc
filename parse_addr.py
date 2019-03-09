49c38758fc235e011993109437cdf0f483989410
Francesco <elettrochuck@gmail.com>
2019-03-09 17:17:18 +0100

    comment and docstrings inserted

--- parse_addr.py
+++ parse_addr.py
@@ -9,17 +9,19 @@ TYPE_STRING = ord('s')
 
 
 # TODO: verifying recursion opportunity
-def add_null(cs):
+def append_null(cs):
+    '''append null values at the end of a string'''
+
     if len(cs) % 4 == 0:
         return cs
     else:
         cs.append(0)
-        return add_null(cs)
+        return append_null(cs)
 
 
 # iterative version of the previous func
 # see previous TODO
-"""def add_null(cs):
+"""def append_null(cs):
     le = 4 - (len(cs) % 4)
     if le != 4:
         for _ in range(le):
@@ -32,6 +34,8 @@ def match_int(n):
 
 
 def int_to_bytes(n):
+    '''convert int to bytes (big endian)'''
+
     conv = struct.pack('>i', n)
     conv = struct.unpack('>BBBB', conv)
     return list(conv)
@@ -42,71 +46,66 @@ def match_float(n):
 
 
 def float_to_bytes(f):
+    '''convert float to bytes (big endian)'''
+
     conv = struct.pack('>f', f)
     conv = struct.unpack('>BBBB', conv)
     return list(conv)
 
 
 def match_string(n):
-    return type(n) == string
+    return type(n) == str
 
 
 def string_to_bytes(s):
+    '''convert string to bytes (big endian)'''
+
     conv = [ord(c) for c in s]
     conv.append(0)
-    return add_null(conv)
+    return append_null(conv)
 
 
-parse_funx = ((match_int, int_to_bytes), (match_float, float_to_bytes),
-              (match_string, string_to_bytes))
-
-'''tps = (int, float, str)
-tps_symbols = (TYPE_INT, TYPE_FLOAT, TYPE_STRING)
-ass = [x for x in zip(tps, tps_symbols)]
-print(ass)'''
+parse_funx = ((match_int, int_to_bytes, TYPE_INT),
+              (match_float, float_to_bytes, TYPE_FLOAT),
+              (match_string, string_to_bytes, TYPE_STRING))
 
 
 def parse_msg(*msg):
-    types = [COMMA]
-    for atom in msg:
-        for match, apply in parse_funx:
-            if match(atom):
-                print(apply)
-                apply(atom)
+    '''parse message arguments and convert them in bytes'''
 
-
-'''def parse_msg(*msg):
     types = [COMMA]
     message = []
-    for i in msg:
-        if type(i) == int:
-            types.append(TYPE_INT)
-            message.extend(int_to_bytes(i))
-        elif type(i) == float:
-            types.append(TYPE_FLOAT)
-            message.extend(float_to_bytes(i))
-        elif type(i) == str:
-            types.append(TYPE_STRING)
-            message.extend(string_to_bytes(i))
-    types.append(0)
-    withnull_msg = add_null(types)
-    withnull_msg.extend(message)
-    return withnull_msg'''
+
+    for atom in msg:
+        for match, apply, byte_type in parse_funx:
+            if match(atom):
+                types.append(byte_type)
+                message.extend(apply(atom))
+
+    with_nulls_msg = append_null(types)
+    with_nulls_msg.extend(message)
+    return with_nulls_msg
 
 
 def parse_address(st):
+    '''parse address and convert it in bytes'''
+
     try:
         address = '(^/{1}[a-zA-Z0-9]+)(/?[a-zA-Z0-9]+)(/?[a-zA-Z0-9]+$)'
+
         if re.search(address, st):
             return string_to_bytes(st)
         else:
             return '''address is not well formed, probably slash symbol (/)
             is not present or it's not in the right place'''
+
     except TypeError as e:
         return e
 
 
-def parse_whole_mess(address, *msg):
+def build_whole_mess(address, *msg):
+    '''build whole message and prepare it to send'''
+
     address_bytes = parse_address(address)
     types = parse_msg(*msg)
     return address_bytes + types
